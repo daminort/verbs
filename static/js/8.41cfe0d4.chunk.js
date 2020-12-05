e6afbca5c8f119aa9f1fc47340b191abdf723586
daminort <daminort@gmail.com>
2020-12-05 12:58:05 +0000

    Deploying to gh-pages from  @ 5a216a5f48e770009458be7fb06546b908b6c30b 🚀

--- static/js/8.41cfe0d4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpverbs=this.webpackJsonpverbs||[]).push([[8],{103:function(e,r,s){"use strict";s.r(r),s.d(r,"PhrasalNew",(function(){return t}));var n=s(3),t=(s(0),function(){return Object(n.jsx)("div",{children:"PhrasalNew"})});r.default=t}}]);
-//# sourceMappingURL=8.41cfe0d4.chunk.js.map
